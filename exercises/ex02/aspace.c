e68f13890879e774434a3f5678bf2fe1ca96f7de
coverney <cassandra.overney@students.olin.edu>
2019-02-04 23:34:53 -0500

    Started HW2

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -3,24 +3,59 @@
 Copyright 2014 Allen Downey
 License: GNU GPLv3
 
+PDF of virtual address space sketch is included in the ex02 folder. It is
+titled "diagrams.PDF".
+
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int var1;
+
+/* Prints address of a local int variable to test if stack grows down
+  With this function, I discovered that the stack grows down towards
+  smaller memory addresses. The address of var2 in decimal form was
+  140721107744428, while the address of var3 in decimal form was
+  140721107744388.
+*/
+void printLocalAddress(){
+  int var3 = 7;
+  printf("Address of local variable is %p\n", &var3);
+}
 
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    /* With second call to malloc, I discovered that heap grows up towards
+    larger memory addresses. The address of p in decimal form was
+    17834000, while the address of c in decimal form was 17834144.
+    */
+    void *c = malloc(128);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
+    printf ("c points to %p\n", c);
     printf ("s points to %p\n", s);
+    printLocalAddress();
+
+    /* Even after generating several different random numbers from 1 to 32,
+    the space between the allocated memory m1 and m2 point to 32 in decimal
+    form. That means there are consistently 4 bytes between the two
+    memory addresses.
+    */
+    srand ( time(NULL) ); // Set random seed
+    int var4 = (rand() % 32) + 1;
+    printf("Random number is %i\n", var4);
+    void *m1 = malloc(var4);
+    void *m2 = malloc(var4);
+    printf ("m1 points to %p\n", m1);
+    printf ("m2 points to %p\n", m2);
 
     return 0;
 }
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -3,6 +3,29 @@
 Copyright 2014 Allen Downey
 License: GNU GPLv3
 
+1) According to stack.c, there are three functions that involve initializing
+arrays and setting each elm in the array of size 5. In foo, each elm is set
+to 42 and supposedly a pointer to the array is returned. In bar, array is set
+to {0, 1, 2, 3, 4}, but nothing is returned. In main, foo is called, bar is
+called, and each elm is printed. I would expect the array from foo to print out.
+
+2) After compiling stack, I got the following error from foo:
+function returns address of local variable. This error means that foo
+is returning the address of a local variable that will be removed from the
+stack diagram after foo's return statement.
+
+3) After running it, I got the following error:
+"Segmentation fault (core dumped)" However, the print statements did execute.
+This error means that the program attempted to access an area of memory
+that it was not allowed to access. After calling foo, when main tried to print
+array it couldn't find the local array variable since it disappeared
+from the stack. The stack diagram can be found in "diagrams.PDF".
+
+4) After commenting out the print statements, I get the same errors. The only
+difference is the fact that the print statements didn't happen.
+
+Moral: don't return pointers to stack allocated data!
+
 */
 
 #include <stdio.h>
@@ -13,30 +36,51 @@ License: GNU GPLv3
 int *foo() {
     int i;
     int array[SIZE];
-
-    printf("%p\n", array);
-
+    //printf("%p\n", array); //I get 0x7fff1ef3a3e0
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
     return array;
 }
 
+/*Working foo: make array static or dynamically allocate array
+int *foo() {
+    int i;
+    static int array[SIZE];
+    // printf("%p\n", array);
+    for (i=0; i<SIZE; i++) {
+        array[i] = 42;
+    }
+    return array;
+}*/
+
 void bar() {
     int i;
     int array[SIZE];
-
-    printf("%p\n", array);
-
+    //printf("%p\n", array); //I get 0x7fff1ef3a3e0
     for (i=0; i<SIZE; i++) {
         array[i] = i;
     }
 }
 
+/*Working bar: input pointer to array
+void bar(int *array) {
+    int i;
+    // printf("%p\n", array);
+    for (i=0; i<SIZE; i++) {
+        array[i] = i;
+    }
+}*/
+
 int main()
 {
     int i;
     int *array = foo();
+
+    /*Code snippet to use working bar()
+    int array[SIZE];
+    bar(array);*/
+
     bar();
 
     for (i=0; i<SIZE; i++) {
